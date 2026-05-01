9bf925f32540a6f23b1469120a7d1524380168c6
Bruno Hugo <brunohugo2007@gmail.com>
2026-05-01 20:34:03 -0300

    feat: logger

---
+++ server/basic/include/Logger.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <sstream>
+
+namespace server{
+namespace basic{
+
+enum class LOG_LEVEL {
+    FATAL, 
+    ERROR,
+    WARNING,
+    INFO, 
+    DEBUG,
+    TRACE
+};
+
+class Logger {
+public:
+    static LOG_LEVEL level;
+    static void write(
+        const char* filename,
+        int line,
+        LOG_LEVEL msglevel,
+        std::string msg
+    );
+    static void setLogLevel(const std::string& debugString);
+};
+
+class LogStream {
+public:
+
+    LogStream(
+        const char *filename,
+        int line,
+        LOG_LEVEL level
+    );
+
+    ~LogStream();
+
+    template<typename T>
+    LogStream& operator<<(const T& log){
+        stream_ << log;
+        return *this;
+    }
+    
+private:
+    LOG_LEVEL level_;
+    std::ostringstream stream_;
+    const char *filename_;
+    uint32_t line_;
+};
+
+
+#define LOG_FATAL       LogStream(__FILE__, __LINE__, LOG_LEVEL::FATAL)
+#define LOG_ERROR       LogStream(__FILE__, __LINE__, LOG_LEVEL::ERROR)
+#define LOG_WARNING     LogStream(__FILE__, __LINE__, LOG_LEVEL::WARNING)
+#define LOG_INFO        LogStream(__FILE__, __LINE__, LOG_LEVEL::INFO)
+#define LOG_DEBUG       LogStream(__FILE__, __LINE__, LOG_LEVEL::DEBUG)
+#define LOG_TRACE       LogStream(__FILE__, __LINE__, LOG_LEVEL::TRACE)
+
+}
+}
---
+++ server/basic/src/Logger.cpp
@@ -0,0 +1,77 @@
+#include "Logger.hpp"
+#include <cstdio>
+#include <string.h>
+#include <chrono>
+#include <ctime>
+#include <unistd.h>
+#include <iostream>
+#include <stdexcept>
+
+using namespace server::basic;
+namespace cr = std::chrono;
+
+static std::string getLevelString(LOG_LEVEL level){
+    switch (level)
+    {
+    case LOG_LEVEL::FATAL:
+        return "FATAL";
+    case LOG_LEVEL::ERROR:
+        return "ERROR";
+    case LOG_LEVEL::WARNING:
+        return "WARNING";
+    case LOG_LEVEL::INFO:
+        return "INFO";
+    case LOG_LEVEL::DEBUG:
+        return "DEBUG";
+    case LOG_LEVEL::TRACE:
+        return "TRACE";
+    }
+}
+
+void Logger::write(
+    const char* filename,
+    int line,
+    LOG_LEVEL msglevel,
+    std::string msg
+){
+    if (level < msglevel) return;
+    auto now = cr::system_clock::now();
+    time_t unixt = cr::system_clock::to_time_t(now);
+    tm* timestamp = localtime(&unixt);
+
+    char buff[32];
+    strftime(buff, sizeof(buff), "%d-%m-%Y %H:%M:%S", timestamp);
+    int output = 1;
+    if ((int)msglevel <= 1) output = 2;
+
+    dprintf(output, "[%s] %s %s:%d %s\n", 
+        buff, 
+        getLevelString(msglevel).c_str(), 
+        filename, 
+        line, 
+        msg.c_str()
+    );
+};
+
+void Logger::setLogLevel(const std::string& debugLevelStr){
+    if (debugLevelStr == "FATAL")           level = LOG_LEVEL::FATAL;
+    else if (debugLevelStr == "ERROR")      level = LOG_LEVEL::ERROR;
+    else if (debugLevelStr == "WARNING")    level = LOG_LEVEL::WARNING;
+    else if (debugLevelStr == "INFO")       level = LOG_LEVEL::INFO;
+    else if (debugLevelStr == "DEBUG")      level = LOG_LEVEL::DEBUG;
+    else if (debugLevelStr == "TRACE")      level = LOG_LEVEL::TRACE;
+    else 
+        throw std::runtime_error("Log level option invalid.");
+}
+
+LOG_LEVEL Logger::level = LOG_LEVEL::INFO;
+
+LogStream::LogStream(
+    const char *filename,
+    int line,
+    LOG_LEVEL level
+): filename_{filename}, line_{line}, level_{level}{};
+
+LogStream::~LogStream(){
+    Logger::write(filename_, line_, level_, stream_.str());
+}
